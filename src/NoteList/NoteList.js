40153932064956f903a13aee19026eb04168fb79
Wojtek Książek <wojtek99955@gmail.com>
2022-04-07 21:02:29 +0200

    Document tittle dynamic notes number

--- src/NoteList/NoteList.js
+++ src/NoteList/NoteList.js
@@ -1,56 +1,66 @@
 import Note from "../Note/Note";
-import React, { useState, useContext } from "react";
-import { Wrapper, ViewWrapper, NoteListWrapper, StyledGridIcon, StyledRowsIcon, SortContainer } from './NoteListStyles';
-import { Context } from '../AppProvider';
-import Sort from '../Sort/Sort';
-
+import React, { useState, useContext, useEffect } from "react";
+import {
+  Wrapper,
+  ViewWrapper,
+  NoteListWrapper,
+  StyledGridIcon,
+  StyledRowsIcon,
+  SortContainer,
+} from "./NoteListStyles";
+import { Context } from "../AppProvider";
+import Sort from "../Sort/Sort";
 
 const NoteList = () => {
+  const ctx = useContext(Context);
 
-    const ctx = useContext(Context);
+  const notes = ctx.filtered.filter((note) => {
+    return note.text.toLowerCase().includes(ctx.searchText);
+  });
 
-    const notes = ctx.filtered.filter((note) => {
-        return (
-            note.text.toLowerCase().includes(ctx.searchText));
-    });
+  const [view, setView] = useState(false);
+  const handleView = () => {
+    setView(!view);
+  };
 
-    const [view, setView] = useState(false);
-    const handleView = () => {
-        setView(!view);
-    };
+  const deleteNote = (id) => {
+    const newNotes = ctx.notes.filter((note) => note.id !== id);
+    ctx.setNotes(newNotes);
+  };
 
-    const deleteNote = (id) => {
-        const newNotes = ctx.notes.filter((note) => note.id !== id);
-        ctx.setNotes(newNotes);
-    };
+  useEffect(() => {
+    document.title = `Notes App - (${ctx.notes.length}) saved notes`;
+  }, [ctx.notes]);
 
-    return (
-        <Wrapper>
-            <SortContainer>
-                <Sort />
-                {
-                    notes.length > 1 && (
-                        <ViewWrapper onClick={handleView}>
-                            {view ? <StyledGridIcon darkMode={ctx.darkMode} /> : <StyledRowsIcon darkMode={ctx.darkMode} />}
-                        </ViewWrapper>
-                    )
-                }
-            </SortContainer>
-            <NoteListWrapper view={view}>
-                {notes.map((note) => (
-                    <Note
-                        id={note.id}
-                        text={note.text}
-                        date={note.date}
-                        handleDeleteNote={deleteNote}
-                        key={note.id}
-                        color={note.color}
-                        done={note.done}
-                    />
-                ))}
-            </NoteListWrapper>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <SortContainer>
+        <Sort />
+        {notes.length > 1 && (
+          <ViewWrapper onClick={handleView}>
+            {view ? (
+              <StyledGridIcon darkMode={ctx.darkMode} />
+            ) : (
+              <StyledRowsIcon darkMode={ctx.darkMode} />
+            )}
+          </ViewWrapper>
+        )}
+      </SortContainer>
+      <NoteListWrapper view={view}>
+        {notes.map((note) => (
+          <Note
+            id={note.id}
+            text={note.text}
+            date={note.date}
+            handleDeleteNote={deleteNote}
+            key={note.id}
+            color={note.color}
+            done={note.done}
+          />
+        ))}
+      </NoteListWrapper>
+    </Wrapper>
+  );
 };
 
 export default NoteList;
--- src/Sort/Sort.js
+++ src/Sort/Sort.js
@@ -1,63 +1,75 @@
-import React, { useContext, useState, useEffect, useRef } from 'react';
-import { Context } from '../AppProvider';
-import { Container, Box, BtnsContainer, DownIcon, AllIcon, DoneIcon, ExIcon } from './SortStyles';
+import React, { useContext, useState, useEffect, useRef } from "react";
+import { Context } from "../AppProvider";
+import {
+  Container,
+  Box,
+  BtnsContainer,
+  DownIcon,
+  AllIcon,
+  DoneIcon,
+  ExIcon,
+} from "./SortStyles";
 
 function Sort() {
+  const boxRef = useRef(null);
 
-    const boxRef = useRef(null);
+  const ctx = useContext(Context);
 
-    const ctx = useContext(Context);
+  const handleShowDone = () => {
+    const filtered = ctx.notes.filter((note) => note.done === true);
+    ctx.setFiltered(filtered);
+  };
 
-    const handleShowDone = () => {
-        const filtered = ctx.notes.filter((note) => note.done === true)
-        ctx.setFiltered(filtered)
+  const showAll = () => {
+    ctx.setFiltered(ctx.notes);
+  };
+
+  const handleNotDone = () => {
+    const filtered = ctx.notes.filter((note) => note.done === false);
+    ctx.setFiltered(filtered);
+  };
+
+  const [showButtons, setShowButtons] = useState(false);
+  const handleShowButtons = (e) => {
+    setShowButtons((prev) => !prev);
+    e.stopPropagation();
+  };
+
+  const handleClickOutside = (e) => {
+    if (boxRef.current && !boxRef.current.contains(e.target)) {
+      setShowButtons(false);
     }
+  };
 
-    const showAll = () => {
-        ctx.setFiltered(ctx.notes);
-    }
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
 
-    const handleNotDone = () => {
-        const filtered = ctx.notes.filter((note) => note.done === false);
-        ctx.setFiltered(filtered);
-    }
-
-    const [showButtons, setShowButtons] = useState(false);
-    const handleShowButtons = (e) => {
-        setShowButtons(prev => !prev);
-        e.stopPropagation();
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
     };
+  }, [showButtons]);
 
-    const handleClickOutside = (e) => {
-        if (boxRef.current && !boxRef.current.contains(e.target)) {
-            setShowButtons(false);
-        }
-    }
-
-    useEffect(() => {
-        document.addEventListener("click", handleClickOutside);
-
-        return () => {
-            document.removeEventListener("click", handleClickOutside);
-        };
-    }, [showButtons]);
-
-    return (
-        <Container>
-            <DownIcon onClick={handleShowButtons} dark={ctx.darkMode} />
-            {showButtons ? (
-                <Box ref={boxRef} showButtons={showButtons}>
-                    <BtnsContainer>
-                        <button onClick={showAll} ><AllIcon /> Wszystkie</button>
-                        <button onClick={handleShowDone} ><DoneIcon />Zrobione</button>
-                        <button onClick={handleNotDone}><ExIcon /> Do zrobienia</button>
-                    </BtnsContainer>
-                </Box>
-            ) : null
-
-            }
-        </Container>
-    );
+  return (
+    <Container>
+      <DownIcon onClick={handleShowButtons} dark={ctx.darkMode} />
+      {showButtons ? (
+        <Box ref={boxRef} showButtons={showButtons}>
+          <BtnsContainer>
+            <button onClick={showAll}>
+              <AllIcon /> Wszystkie
+            </button>
+            <button onClick={handleShowDone}>
+              <DoneIcon />
+              Zrobione
+            </button>
+            <button onClick={handleNotDone}>
+              <ExIcon /> Do zrobienia
+            </button>
+          </BtnsContainer>
+        </Box>
+      ) : null}
+    </Container>
+  );
 }
 
 export default Sort;
